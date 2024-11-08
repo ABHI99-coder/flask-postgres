10ccb3febab2a7fa5cb9be77c3bbd033f30dab11
ABHI99-coder <64468233+ABHI99-coder@users.noreply.github.com>
2024-11-08 14:01:18 +0530

    Update __init__.py

--- todo/__init__.py
+++ todo/__init__.py
@@ -8,7 +8,7 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__,static_folder=os.path.abspath('static'))
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
-    'DATABASE_URL', 'postgresql://todo_user:your_password@postgres/todo_db'
+    'DATABASE_URL', 'postgresql://todo_user:redhat1234#@flask-post.postgres.database.azure.com:5432/postgres'
 )
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
