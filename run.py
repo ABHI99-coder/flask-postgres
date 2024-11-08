b6a511fd01e5f5b6884105f0d12a947ce883f906
ABHI99-coder <sonupapa1999@gmail.com>
2024-11-08 13:20:00 +0530

    first commit

---
+++ run.py
@@ -0,0 +1,15 @@
+# run.py
+
+
+from todo import db, create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    
+    app.run(debug=True, host="0.0.0.0", port=5002)
+
+
+
