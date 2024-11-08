1c76f80bba67e17e53ce21853e27e8f1796b279c
ABHI99-coder <64468233+ABHI99-coder@users.noreply.github.com>
2024-11-08 13:56:16 +0530

    Update run.py

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     
-    app.run(debug=True, host="0.0.0.0", port=5002)
+    app.run(debug=True, host="0.0.0.0", port=80)
 
 
 
